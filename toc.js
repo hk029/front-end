a425c6a72080c11a75dd8b912f27906a861342c4
KaiHuang <kaihuang@sohu-inc.com>
2017-07-16 14:20:39 +0800

    add

--- toc.js
+++ toc.js
@@ -10,15 +10,24 @@ var readDir = require('./readDir.js')
 //   filters: [/\.git/g]
 // })
 var data = fs.readFileSync('./test.md');
-var reg = /^(#+.*)/gm
+var reg = /^(#+.*)/gm;
 var match;
 var toc = '目录\n';
+var firstLevel = 0;
+
 while((match = reg.exec(data)) != null){
+  let reg2 = /^(#+)/g
   var title = match[1];
+  var level = reg2.exec(title)[1].length; 
+  if(!firstLevel){
+    firstLevel = level;
+  }
+  // console.log(title,level);
   var url = title.replace(/#+\s/g,'');
-  title = title.replace(url,`[${url}](#${url})`);
+  title ='  '.repeat(level-firstLevel) + `- [${url}](#${url})`;
   toc += title + '\n';
 }
+console.log(toc);
 data  = toc + data;
 
-fs.writeFileSync('./test.md',data);
+// fs.writeFileSync('./test.md',data);
